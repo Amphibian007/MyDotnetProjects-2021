9cfdc283b69b369fd9b02d79c4ccf4baa03ffd1e
Dipto Das <diptod111@gmail.com>
2021-02-11 22:19:44 +0600

    Added project 1 Passion store(1)

---
+++ Udemy Project Part1/PassionStore/PassionStore/Controllers/WebsiteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassionStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassionStore.Controllers
+{
+    public class WebsiteController : Controller
+    {
+        public IActionResult Details(string slugLink)
+        {
+            var website = WebRepository.GetById(slugLink);
+            return View(website);
+        }
+        public IActionResult ListOFWebsite()
+        {
+            var websites = WebRepository.GetAll();
+            return View(websites);
+        }
+    }
+}
---
+++ Udemy Project Part1/PassionStore/PassionStore/Models/WebRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassionStore.Models
+{
+    public class WebRepository
+    {
+        public static List<Website> GetAll()
+        {
+            var allWebsites = new List<Website>
+            {
+                new Website
+                {
+                    Id=1,
+                    Name="Google",
+                    Price=20
+                },
+                new Website
+                {
+                    Id=2,
+                    Name="Amazon",
+                    Price=30
+                },
+                 new Website
+                 {
+                    Id=3,
+                    Name="Microsoft",
+                    Price=50
+                 },
+                  new Website
+                {
+                    Id=4,
+                    Name="Foodpanda",
+                    Price=60
+                } ,
+                  new Website
+                {
+                    Id=5,
+                    Name="Apple",
+                    Price=20
+                } ,
+                  new Website
+                {
+                    Id=6,
+                    Name="Product Of The Month",
+                    Price=20
+                },
+                new Website
+                {
+                    Id=6,
+                    Name="Machintosh",
+                    Price=20
+                }
+            };
+            return allWebsites;
+        }
+        public static Website GetById(string slug)
+        {
+            List<Website> websites = WebRepository.GetAll();
+            foreach(var website in websites)
+            {
+                if (website.Slug == slug)
+                {
+                    return website;
+                }
+               
+            }
+            return null;
+        }
+    }
+}
