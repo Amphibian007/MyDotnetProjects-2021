996a83561030d4af7af6ce272d44bf7b407a5099
Dipto Das <diptod111@gmail.com>
2021-02-12 00:06:01 +0600

    Added Project 1 named Sharee.cs(2)

---
+++ Udemy Project Part1/ShareeStore/ShareeStore/Controllers/ShareeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShareeStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareeStore.Controllers
+{
+    public class ShareeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var sharees = ShareeRepository.GetAll();
+            return View(sharees);
+        }
+        public IActionResult Details(string slug)
+        {
+            var sharee = ShareeRepository.GetSingleItem(slug);
+            return View(sharee);
+        }
+    }
+}
---
+++ Udemy Project Part1/ShareeStore/ShareeStore/Models/ShareeRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareeStore.Models
+{
+    public class ShareeRepository
+    {
+        public static List<Sharee> GetAll()
+        {
+            List<Sharee> allSharees = new List<Sharee>()
+        {
+            new Sharee
+            {
+                Id=1,
+                Name="Jamdani",
+                Category="Homemade"
+            },
+            new Sharee
+            {
+                Id=2,
+                Name="Tati",
+                Category="Tangail"
+            },
+            new Sharee
+            {
+                Id=3,
+                Name="Silk",
+                Category="Indian"
+            },
+            new Sharee
+            {
+                Id=4,
+                Name="Khadi",
+                Category="Comilla"
+            },
+             new Sharee
+            {
+                Id=4,
+                Name="Sharee of the day",
+                Category="Comilla"
+            }
+        };
+            return allSharees;
+        }
+
+        public static Sharee GetSingleItem(string slug)
+        {
+            var sharees = ShareeRepository.GetAll();
+            foreach(var item in sharees)
+            {
+                if (item.slugLink == slug)
+                {
+                    return item;
+                }                
+            }
+            return null ;
+        }
+    }
+}
