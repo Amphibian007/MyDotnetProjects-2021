1dd555bd71d40e9e118a5fbc2fbc1545f0315247
Dipto Das <diptod111@gmail.com>
2021-02-13 01:06:33 +0600

    Added project 2 initially (2)

---
+++ Udemy Project Part2/InvestmentCalculator/InvestmentCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InvestmentCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Udemy Project Part2/InvestmentCalculator/InvestmentCalculator/Models/InvestmentModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestmentCalculator.Models
+{
+    public class InvestmentModel
+    {
+        [Required(ErrorMessage ="This field is required")]     
+        [Range(1,10000,ErrorMessage ="Value should be from 1 to 10000")]
+        public decimal? MonthlyInvestment { get; set; }
+        [Required(ErrorMessage ="This field is required")]
+        [Range(.1, 5, ErrorMessage = "Value should be from .1 to 5")]
+        public decimal? YearlyInterestRate { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [Range(1,5,ErrorMessage ="Year should be from less than 5")]
+        public int? Years { get; set; }
+        public decimal? CalculateInvestment()
+        {
+            int? months = Years * 12;
+            decimal? monthlyInterestRate = YearlyInterestRate / 100 / 12;
+            decimal? futureValue = 0;
+            for(int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+            return futureValue;
+        }
+    }
+}
