ca55a1812fe6ef7a2cbf6648f89caa410ce36634
Dipto Das <diptod111@gmail.com>
2021-02-14 00:38:04 +0600

    I have done it finally

---
+++ Udemy Project Part2/SumCalculator/SumCalculator/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SumCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SumCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Result = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Digit value)
+        {
+            if (ModelState.IsValid)
+            {
+                if (value.Operation == "+")
+                {
+                    ViewBag.Result = value.Sum().ToString("c2");
+                }
+                else if (value.Operation == "-")
+                {
+                    ViewBag.Result = value.Sub().ToString("c2");
+                }
+                else if (value.Operation == "*")
+                {
+                    ViewBag.Result = value.Mul().ToString("c2");
+                }
+                else if (value.Operation == "/")
+                {
+                    ViewBag.Result = value.Div().ToString("c2");
+                }
+            }
+            else
+            {
+                ViewBag.Result = "";
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Udemy Project Part2/SumCalculator/SumCalculator/Models/Digit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SumCalculator.Models
+{
+    public class Digit
+    {
+        public decimal Num1 { get; set; }
+        public decimal Num2 { get; set; }
+        public string Operation { get; set; }
+        public decimal Sum()
+        {
+            decimal result = Num1 + Num2;
+            return result;
+        }
+        public decimal Sub()
+        {
+            decimal result = Num1 - Num2;
+            return result;
+        }
+        public decimal Mul()
+        {
+            decimal result = Num1 * Num2;
+            return result;
+        }
+        public decimal Div()
+        {
+            decimal result = Num1 / Num2;
+            return result;
+        }
+    }
+}
