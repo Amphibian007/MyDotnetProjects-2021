9a923662064bee4049163f29dab7da6c305f6357
Dipto Das <diptod111@gmail.com>
2021-02-13 22:59:22 +0600

    Added MoneyCalculator (Practise 2)

---
+++ Udemy Project Part2/MoneyCalculator/MoneyCalculator/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MoneyCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.futureMoney = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(InvestmentModel value)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.futureMoney = value.Calculate().ToString("c2");
+            }
+            else
+            {
+                ViewBag.futureMoney = "";
+            }
+            return View();        
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Udemy Project Part2/MoneyCalculator/MoneyCalculator/Models/InvestmentModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyCalculator.Models
+{
+    public class InvestmentModel
+    {
+        [Required(ErrorMessage ="This is required man")]
+        [Range (1, 3000, ErrorMessage ="Btweeen 1 to 3000")]
+        public decimal? MonthlyInvestment { get; set; }
+        [Required(ErrorMessage ="This rate is also required man")]
+        [Range(3,5, ErrorMessage = "Btweeen 3 to 5")]
+        public decimal? YearlyInterestRate { get; set; }
+        [Required(ErrorMessage ="Enter your years")]
+        [Range(1, 5, ErrorMessage = "Should be less than 5")]
+        public int? Years { get; set; }
+        public decimal Calculate()
+        {
+            decimal months = Years.Value * 12;
+            decimal monthlyInterestRate = YearlyInterestRate.Value / 100 / 12;
+            decimal futureMoney = 0;
+            for(int i = 0; i < months; i++)
+            {
+                futureMoney = (futureMoney + MonthlyInvestment.Value) * (1 + monthlyInterestRate);
+            }
+            return futureMoney;
+        }
+    }
+}
