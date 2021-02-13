096c41571ffba95d7662f3d65b1a34a239f8a907
Dipto Das <diptod111@gmail.com>
2021-02-14 00:06:15 +0600

    Added AssetCalculator Practise(4)

---
+++ Udemy Project Part2/AssetCalculator/AssetCalculator/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using AssetCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.futureValue = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Asset value)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.futureValue = value.Calculate().ToString("c2");
+            }
+            else
+            {
+                ViewBag.futureValue = "";
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Udemy Project Part2/AssetCalculator/AssetCalculator/Models/Asset.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetCalculator.Models
+{
+    public class Asset
+    {
+        [Required(ErrorMessage ="You should enter monthly asset")]
+        [Range(1,5000,ErrorMessage ="Between 1 to 5000")]
+        public decimal? MonthlyAsset { get; set; }
+        [Required(ErrorMessage = "You should enter yearly interest rate")]
+        [Range(.1, 6, ErrorMessage = "Between .1 to 6")]
+        public decimal? YearlyInterestRate { get; set; }
+        [Required(ErrorMessage = "You should enter years")]
+        [Range(1, 5, ErrorMessage = "Less than 5")]
+        public int? Years { get; set; }
+        public decimal Calculate()
+        {
+            decimal months = Years.Value * 12;
+            decimal monthlyInerestRate = YearlyInterestRate.Value / 100 / 12;
+            decimal futurValue = 0;
+            for(int i=0;i<months; i++)
+            {
+                futurValue = (futurValue + MonthlyAsset.Value) * (1 + monthlyInerestRate);
+            }
+            return futurValue;
+
+        }
+    }
+}
