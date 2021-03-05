e259cd393d8e188e93e1ac638c382896a983f014
Dipto Das <diptod111@gmail.com>
2021-03-06 00:17:00 +0600

    Added for fifth time

---
+++ Udemy Project Part1/ProductStore/ProductStore/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult List()
+        {
+            var products = ProductRepository.GetAllProducts();
+            return View(products);
+        }
+        public IActionResult Details(string slug)
+        {
+            var product = ProductRepository.GetSingleProduct(slug);
+            
+            return View(product);
+        }
+    }
+}
---
+++ Udemy Project Part1/ProductStore/ProductStore/Models/ProductRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductStore.Models
+{
+    public class ProductRepository
+    {
+        public static List<Product> GetAllProducts()
+        {
+            List<Product> allProducts = new List<Product>
+            {
+                new Product
+                {
+                    ProductId=1,
+                    Name="Mobile",
+                    Price=(decimal)98.6
+                },
+                new Product
+                {
+                    ProductId=2,
+                    Name="Book",
+                    Price=(decimal)108.6
+                },
+                new Product
+                {
+                    ProductId=3,
+                    Name="Bat",
+                    Price=(decimal)500.6
+                },
+                new Product
+                {
+                    ProductId=4,
+                    Name="Ball",
+                    Price=(decimal)30.6
+                },
+                new Product
+                {
+                    ProductId=5,
+                    Name="Diary",
+                    Price=(decimal)120.6
+                },
+                  new Product
+                {
+                    ProductId=6,
+                    Name="Product of the month",
+                    Price=(decimal)100.6
+                }
+            };
+            return allProducts;
+        }
+        public static Product GetSingleProduct(string slug)
+        {
+            var products = ProductRepository.GetAllProducts();
+            foreach(var item in products)
+            {
+                if (item.Slug == slug)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+}
