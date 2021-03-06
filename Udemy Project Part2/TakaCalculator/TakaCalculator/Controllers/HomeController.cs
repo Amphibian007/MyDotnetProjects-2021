95bb4a7760c9da06ad9083218957a7109fd777b1
Dipto Das <diptod111@gmail.com>
2021-03-06 11:55:14 +0600

    Added TakaCalculator(Practise 7)

---
+++ Udemy Project Part2/TakaCalculator/TakaCalculator/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TakaCalculator.Models;
+
+namespace TakaCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FutureTaka = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(TakaModel value)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FutureTaka = value.CalculateTaka().ToString("c0");
+            }
+            else
+            {
+                ViewBag.FutureTaka = "";
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Udemy Project Part2/TakaCalculator/TakaCalculator/Models/TakaModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakaCalculator.Models
+{
+    public class TakaModel
+    {
+        [Required(ErrorMessage ="Monthly investment is required")]
+        public decimal? MonthlyInvestment { get; set; }
+
+        [Required(ErrorMessage = "Yearly interest rate is required")]
+        public decimal? YearlyInterestRate { get; set; }
+
+        [Required(ErrorMessage = "Years required")]
+        public decimal? Years { get; set; }
+
+        public decimal CalculateTaka()
+        {
+            var months = Years.Value * 12;
+            var monthlyInterestRate = YearlyInterestRate.Value / 100 / 12;
+            decimal futureTaka = 0;
+            for(int i = 0; i < months; i++)
+            {
+                futureTaka = (futureTaka + MonthlyInvestment.Value) * (1 + monthlyInterestRate);
+            }
+            return futureTaka;
+        }
+
+    }
+}
